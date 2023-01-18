6009b092540442b298067b5d36d547463b389aa3
Yusuf Yıldız <114533908+YusufIlyasy@users.noreply.github.com>
2023-01-18 20:12:01 +0300

    Delete inc-fonsiyon.php

--- inc-fonsiyon.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    try {
-        $conn = new PDO("mysql:host=localhost;dbname=blog_db;charset=utf8", 'root', '');
-    } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-        die();
-    } 
-    define("_URL","http://localhost/final_ip/");
-?>
